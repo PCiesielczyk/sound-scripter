dafd10182f7105c188b8834dd69c5dd929512d1b
PCiesielczyk <piotrciesielczyk2001@gmail.com>
2024-02-13 21:43:04 +0100

    feat(audio capture): register capture activity and prompt for user grant

--- app/src/main/java/com/example/sound_scripter/MainActivity.kt
+++ app/src/main/java/com/example/sound_scripter/MainActivity.kt
@@ -1,10 +1,22 @@
 package com.example.sound_scripter
 
+import android.media.projection.MediaProjection
+import android.media.projection.MediaProjectionManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
@@ -13,8 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.AlignmentLine
-import androidx.compose.ui.layout.HorizontalAlignmentLine
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,6 +35,20 @@ import com.example.sound_scripter.ui.theme.SoundScripterTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val mediaProjectionManager = getSystemService(MediaProjectionManager::class.java)
+        var mediaProjection: MediaProjection
+
+        val startMediaProjection = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == RESULT_OK) {
+                mediaProjection = mediaProjectionManager
+                    .getMediaProjection(result.resultCode, result.data!!)
+            }
+        }
+        startMediaProjection.launch(mediaProjectionManager.createScreenCaptureIntent())
+
         setContent {
             SoundScripterTheme {
                 // A surface container using the 'background' color from the theme
@@ -51,6 +76,7 @@ fun TranscriptDisplay(modifier: Modifier = Modifier) {
         val onEnabled: (Boolean) -> Unit = {
             enabled = it
             displayedText = if (it) listeningDisplayText else initialDisplayText
+
         }
 
         IconToggleButton(checked = enabled,
